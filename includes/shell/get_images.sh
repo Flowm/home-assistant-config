61d66ba2218fda361e8848f2915481af1dc52b35
Florian Mauracher <florian.mauracher@gmx.net>
2017-07-29 04:04:38 +0200

    Add first webcam image

---
+++ includes/shell/get_images.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Config
+OUT_DIR="/home/homeassistant/.homeassistant/media/"
+mkdir -p $OUT_DIR
+cd $OUT_DIR
+
+# Get images
+wget -qN "http://www.dwd.de/DWD/wetter/radar/rad_bay_akt.jpg" -O rad_bay_akt.jpg
+
+wget -q "https://www.foto-webcam.eu/webcam/muenchen/$(date +%Y/%m/%d)/$(date +%H%M | cut -c-3)0_hd.jpg" -O web_freimann.jpg
